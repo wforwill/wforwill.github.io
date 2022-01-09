81078df463fd86eb284a50bf28b72ea83588544b
wforwill <92567611+wforwill@users.noreply.github.com>
2022-01-09 16:19:41 +0800

    Update script.js

--- final-project/script.js
+++ final-project/script.js
@@ -1,32 +1,34 @@
 $(function () {
-    // $(window).scroll(function () {
-    //     var scrolled = $(document).scrollTop();
-    //     var winh1 = $(window).height();
-    //     $(".opa").each(function () {
-    //         var opRange = $(this).parent().parent().height() / 3;
-    //         $(this).css({
-    //             opacity: ($(this).data("seq") * opRange - scrolled + winh1 * 2) / opRange
-    //         });
-    //     });
-    // });
 
-    // var video_pos = $("#bloom").offset().top;
-    // var video_end = $("#bloom").offset().top + $("#bloom").height();
-    // $(window).scroll(function () {
-    //     var current_pos3 = $(window).scrollTop();
-    //     if (current_pos3 > video2_pos && current_pos3 < video2_end) {
-    //         $("#video")[0].play();
-    //     } else {
-    //         $("#video")[0].pause();
-    //     }
-    // });
-
-    var doch = $(document).height();
-    var winw = $(window).width();
-    var winh2 = $(window).height();
     $(window).scroll(function () {
-        var current_pos3 = $(window).scrollTop();
-        var new_width = (current_pos3 - $("#doubt1").offset().top) / ($("#doubt1").height() - winh2) * winw;
+        var new_width = ($(window).scrollTop() - $("#doubt1").offset().top) / ($("#doubt1").height() - $(window).height()) * $(window).width();
         $("#fakeprogressbar").css("width", new_width);
     });
+
+    $(window).scroll(function () {
+        $(".districts").each(function () {
+            var opRange = $(window).height();
+            $(this).css({
+                opacity: ($(this).data("seq") * opRange - $(document).scrollTop() + $("#doubt2").offset().top) / opRange
+            });
+        });
+    });
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > $("#bloom").offset().top && $(window).scrollTop() < $("#bloom").offset().top + $("#bloom").height()) {
+            $("#video")[0].play();
+        } else {
+            $("#video")[0].pause();
+        }
+    });
+
+    $(window).scroll(function () {
+        $(".news").each(function () {
+            var opRange = $(window).height();
+            $(this).css({
+                opacity: ($(this).data("seq") * opRange - $(document).scrollTop() + $("#too-many-news").offset().top + $(window).height()) / opRange
+            });
+        });
+    });
+
 });
