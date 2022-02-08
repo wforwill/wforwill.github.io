5f40da7c721c4f3057f5cf7a0f5bd41841c37745
wforwill <92567611+wforwill@users.noreply.github.com>
2022-02-08 11:56:35 +0800

    Update script.js

--- assignment6/script.js
+++ assignment6/script.js
@@ -1,68 +1,8 @@
+var s = skrollr.init();
+
 $(window).scroll(function () {
     var scrolled = $(document).scrollTop();
     var winh = $(window).height();
-
-    // code to hide/show grouped items
-    if (scrolled >= $("#maploc1").offset().top-winh * 0.4){
-        $("#Layer_3").css({
-            "opacity": "0"
-        });
-    }
-    else {
-        $("#Layer_3").css({
-            "opacity": "1"
-        });
-    }
-    if (scrolled >= $("#maploc2").offset().top-winh * 0.4){
-        $("#Layer_2").css({
-            "opacity": "0"
-        });
-    }
-    else {
-        $("#Layer_2").css({
-            "opacity": "1"
-        });
-    }
-    if (scrolled >= $("#maploc3").offset().top-winh * 0.4){
-        $("#Layer_1").css({
-            "opacity": "0"
-        });
-    }
-    else {
-        $("#Layer_1").css({
-            "opacity": "1"
-        });
-    }
-    if (scrolled >= $("#maploc4").offset().top-winh * 0.4){
-        $("#Layer_4").css({
-            "opacity": "1"
-        });
-    }
-    else {
-        $("#Layer_4").css({
-            "opacity": "0"
-        });
-    }
-    if (scrolled >= $("#maploc5").offset().top-winh * 0.4){
-        $("#Layer_5").css({
-            "opacity": "1"
-        });
-    }
-    else {
-        $("#Layer_5").css({
-            "opacity": "0"
-        });
-    }
-    if (scrolled >= $("#maploc6").offset().top-winh * 0.4){
-        $("#Layer_6").css({
-            "opacity": "1"
-        });
-    }
-    else {
-        $("#Layer_6").css({
-            "opacity": "0"
-        });
-    }
     if (scrolled >= $("#maploc7").offset().top-winh * 0.4){
         $("#Layer_7").css({
             "opacity": "1"
@@ -81,26 +21,14 @@ $(window).scroll(function () {
         $("#Layer_7").css({
             "opacity": "0"
         });
-    }
-    if (scrolled >= $("#maploc8").offset().top-winh * 0.4){
-        $("#Layer_8").css({
-            "opacity": "1"
+        $("#Layer_1").css({
+            "opacity": "0"
         });
-    }
-    else {
-        $("#Layer_8").css({
+        $("#Layer_2").css({
+            "opacity": "0"
+        });
+        $("#Layer_3").css({
             "opacity": "0"
         });
     }
-    if (scrolled >= $("#maploc9").offset().top-winh * 0.4){
-        $("#Layer_9").css({
-            "opacity": "1"
-        });
-    }
-    else {
-        $("#Layer_9").css({
-            "opacity": "0"
-        });
-    }
-    
 });
